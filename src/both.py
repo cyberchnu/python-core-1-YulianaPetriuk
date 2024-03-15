e51f40bdf6a3aaaadfbdbdffd114bf1666f83aa5
Юліана <petriuk.yuliana@chnu.edu.ua>
2024-03-15 12:33:20 +0200

    both.py

--- src/both.py
+++ src/both.py
@@ -1,5 +1,5 @@
 def both(number1, number2):
-  if number1 > 0 and number2 > 0 or number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0:
-    return True
-  else:
-    return False
+    if number1 > 0 and number2 > 0 or number1 < 0 and number2 < 0 or number1 == 0 and number2 == 0:
+        return True
+    else:
+        return False
