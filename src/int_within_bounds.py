a82c0e3871fb85c739ed9b3600349c16f5b68277
Юліана <petriuk.yuliana@chnu.edu.ua>
2024-03-27 17:30:00 +0200

    int_within_bounds.py2

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,8 +1,7 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  if type(number) != int:
-    return False
-  elif number < upper_bound and number >= lower_bound:
-    return True
-  else:
-    return False
-
+    if type(number) != int:
+        return False
+    elif number < upper_bound and number >= lower_bound:
+        return True
+    else:
+        return False
